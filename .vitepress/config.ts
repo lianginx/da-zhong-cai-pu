8c7540c0d4a2e4efb05cd7abb7bae6f927197c89
Liang INX <liang@in-x.cc>
2024-04-22 00:06:30 +0800

    引入 Markdown 注脚插件

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -1,19 +1,25 @@
 import { defineConfig } from "vitepress";
+import footnote from "markdown-it-footnote";
 import sidebar from "./sidebar";
 import nav from "./nav";
 
 export default defineConfig({
   title: "大众菜谱",
   description: "集合中国各地特色菜肴的烹饪书籍",
+  markdown: {
+    config(md) {
+      md.use(footnote);
+    },
+  },
   themeConfig: {
     nav,
     sidebar,
-    search: { provider: 'local' },
+    search: { provider: "local" },
     socialLinks: [
       { icon: "github", link: "https://github.com/lianginx/da-zhong-cai-pu" },
     ],
     footer: {
-      message: '基于 MIT 许可发布',
-    }
+      message: "基于 MIT 许可发布",
+    },
   },
 });
