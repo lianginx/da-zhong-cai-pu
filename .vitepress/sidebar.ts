8fa43ccab03d30041ce2141e177189f1617c1cf7
Liang <liang@in-x.cc>
2025-11-10 12:40:53 +0800

    📝 菜谱：生煸菠菜（上海）

--- .vitepress/sidebar.ts
+++ .vitepress/sidebar.ts
@@ -4,20 +4,21 @@ export default <DefaultTheme.Sidebar>[
   { text: "再版说明", link: "/reprint-notice" },
   {
     text: "蔬菜类",
-    base: "vegetables",
+    base: "/vegetables",
     collapsed: false,
     items: [
       {
-        text: '上海',
+        text: "上海",
         collapsed: true,
         items: [
           { text: "开洋烧菜心", link: "/shanghai/kai-yang-shao-cai-xin" },
           { text: "虾油菠菜", link: "/shanghai/xia-you-bo-cai" },
+          { text: "生煸菠菜", link: "/shanghai/sheng-bian-bo-cai" },
         ],
       },
       {
-        text: '杭州',
-        base: 'hangzhou',
+        text: "杭州",
+        base: "hangzhou",
         collapsed: true,
         items: [
           // { text: "开洋烧菜心", link: "/kai-yang-shao-cai-xin" },
@@ -28,19 +29,19 @@ export default <DefaultTheme.Sidebar>[
   },
   {
     text: "豆腐面筋类",
-    base: "tofu-gluten",
+    base: "/tofu-gluten",
     collapsed: false,
     items: [
       {
-        text: '上海',
+        text: "上海",
         collapsed: true,
         items: [
           { text: "辣酱豆腐", link: "/shanghai/la-jiang-dou-fu" },
           { text: "家常豆腐", link: "/shanghai/jia-chang-dou-fu" },
-        ]
+        ],
       },
       {
-        text: '杭州',
+        text: "杭州",
         collapsed: true,
         items: [],
       },
@@ -48,19 +49,19 @@ export default <DefaultTheme.Sidebar>[
   },
   {
     text: "肉类",
-    base: "meat",
+    base: "/meat",
     collapsed: false,
     items: [
       {
-        text: '上海',
+        text: "上海",
         collapsed: true,
         items: [
           { text: "韭芽炒肉丝", link: "/shanghai/jiu-ya-chao-rou-si" },
           { text: "炒姜丝肉", link: "/shanghai/chao-jiang-si-rou" },
-        ]
+        ],
       },
       {
-        text: '杭州',
+        text: "杭州",
         collapsed: true,
         items: [],
       },
@@ -68,31 +69,31 @@ export default <DefaultTheme.Sidebar>[
   },
   {
     text: "禽蛋类",
-    base: "poultry-eggs",
+    base: "/poultry-eggs",
     collapsed: false,
     items: [],
   },
   {
     text: "鱼虾类",
-    base: "fish-shrimp",
+    base: "/fish-shrimp",
     collapsed: false,
     items: [],
   },
   {
     text: "凉菜类",
-    base: "cold-dishes",
+    base: "/cold-dishes",
     collapsed: false,
     items: [],
   },
   {
     text: "汤菜类",
-    base: "soup",
+    base: "/soup",
     collapsed: false,
     items: [],
   },
   {
     text: "附录",
-    base: "appendix",
+    base: "/appendix",
     collapsed: false,
     items: [],
   },
